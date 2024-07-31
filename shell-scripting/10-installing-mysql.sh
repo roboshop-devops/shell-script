bacdf40e80a2056458e6f1a977fe6505e8720c04
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 20:12:32 +0530

    installing mysql and git

--- shell-scripting/10-installing-mysql.sh
+++ shell-scripting/10-installing-mysql.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-    ID=(id -u)
+ID=$(id -u)
 
-    if [ $ID -ne 0 ]
-    then
-        echo "ERROR :: Please run this script with root access"
-        EXIT 1
-    else
-        echo "You are root user"
-     fi
+if [ $ID -ne 0 ]
+then
+    echo "ERROR :: Please run this script with root access"
+    EXIT 1
+else
+    echo "You are root user"
+fi
 
-        yum install mysql -y
+yum install mysql -y
 
-     if [ $? -ne o ]
-    then
-            echo "ERROR::Installation of MYSQL failed"
-            EXIT 1
-     else
-            echo "echo "Installed of MYSQL successfully"     
-    fi
+if [ $? -ne o ]
+then
+    echo "ERROR::Installation of MYSQL failed"
+    EXIT 1
+else
+    echo "echo "Installed of MYSQL successfully"     
+fi
 
-        yum isntall git -y
+yum isntall git -y
 
-    if [ $? -ne 0]
-    then
-        echo "ERROR::Installation of GIT failed"
-        EXIT 1
-    else
-            echo"Installed of GIT successfully"     
-    fi
-
+if [ $? -ne 0 ]
+then
+    echo "ERROR::Installation of GIT failed"
+    EXIT 1
+else
+    echo "Installed of GIT successfully"     
+fi
