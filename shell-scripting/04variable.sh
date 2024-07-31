a19de0046c57b53f570845c075a68605d2f3f215
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 18:02:25 +0530

    git variable practicing now for 2nd time

--- shell-scripting/04variable.sh
+++ shell-scripting/04variable.sh
@@ -5,8 +5,12 @@ PERSON2=$2
 PERSON3=$3
 
 
-echo "$PERSON1 :: hi $PERSON3, how are you doing" 
-echo "$PERSON3 :: helloo $PERSON1, im good"
+echo "$PERSON1 :: hi $PERSON2, how are you doing" 
+echo "$PERSON2 :: helloo $PERSON1, im good"
 echo "$PERSON3 :: hi $PERSON1 and $person2 not seen from last few dats"
 echo "$PERSON1 :: hello $PERSON3 i was busy with work,how are you now"
 echo "$PERSON2 :: yes $PERSON3 i was out of station."
+
+
+
+
