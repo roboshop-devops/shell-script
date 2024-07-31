6132fb71f10aba197a0bc190b87bc3fad666ef0d
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 18:05:15 +0530

    git variable practicing now for 3rd time

---
+++ shell-scripting/05variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1 :: hi $PERSON2, how are you doing" 
+echo "$PERSON2 :: helloo $PERSON1, im good"
+echo "$PERSON1 :: hello $PERSON2 i was busy with work,how are you now"
+echo "$PERSON2 :: yes $PERSON1 i was out of station"
+
