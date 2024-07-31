7adb03ce4425a81b4979b77d556e2669ec6f3777
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 21:03:40 +0530

    installing mysql with colours

--- shell-scripting/11-installationdemo.sh
+++ shell-scripting/11-installationdemo.sh
@@ -2,8 +2,8 @@
 
 ID=$(id -u)
 
-R="\e[0;31m"
-G="\e[0;32m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 
 if [ $ID -ne 0 ]
@@ -11,25 +11,25 @@ then
     echo -e "ERROR:: Please run this script with root access"
     exit 1 # you can give other than 0
 else
-    echo -e"$G You are root user $N"
+    echo -e "$G You are root user $N"
 fi # fi means reverse of if, indicating condition end
 
 yum install mysqll -y
 
 if [ $? -ne 0 ]
 then
-    echo -e"ERROR:: $R Installing MySQL is failed $N"
+    echo -e "ERROR:: $R Installing MySQL is failed $N"
     exit 1
 else
-    echo -e"$G Installing MySQL is SUCCESS $N"
+    echo -e "$G Installing MySQL is SUCCESS $N"
 fi
 
 yum install gitt -y
 
 if [ $? -ne 0 ]
 then
-    echo -e"ERROR:: $R Installing GIT is failed $N"
+    echo -e "ERROR:: $R Installing GIT is failed $N"
     exit 1
 else
-    echo -e"$G Installing GIT is SUCCESS $N"
+    echo -e "$G Installing GIT is SUCCESS $N"
 fi
