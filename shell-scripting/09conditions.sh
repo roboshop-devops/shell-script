2e1e1d2bc3225e00ec19fa04408d4757b8dec17a
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 18:47:58 +0530

    git conditions practice

--- shell-scripting/09conditions.sh
+++ shell-scripting/09conditions.sh
@@ -4,6 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -ge 100]
 then
-echo "given number $NUMBER is greater than 100"
+    echo "given number $NUMBER is greater than 100"
 else
-echo "given number $NUMBER is not greater than 100"
+    echo "given number $NUMBER is not greater than 100"
+fi
