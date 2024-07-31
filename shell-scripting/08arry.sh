ea3e50b003e8aa3d0dca81f2c0e83133a1c584aa
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-07-31 18:34:31 +0530

    git arry practice

---
+++ shell-scripting/08arry.sh
@@ -0,0 +1,10 @@
+#1/bin/nbash
+
+ANIMALS=("dog" "cow" "bull" "buffallo")
+
+echo "1st argumentis ${ANiMALS[0]}"
+echo "2nd argumentis ${ANiMALS[1]}"
+echo "3rd argumentis ${ANiMALS[2]}"
+echo "4th argumentis ${ANiMALS[3]}"
+echo "total no of arguments $#"
+echo  "all arguments $@"
