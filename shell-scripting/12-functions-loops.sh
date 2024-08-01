b0c6bdcaac84f1b2d7731a8b087d8967e895889c
Nagarjun_Devops <arjunreddy943@gmail.com>
2024-08-01 11:52:36 +0530

    loops 1st commit

--- shell-scripting/12-functions-loops.sh
+++ shell-scripting/12-functions-loops.sh
@@ -31,7 +31,7 @@ fi
 for package in $@ 
 do
     yum list installed package &>> $LOGFILE
-    if [ $? -ne 0]
+    if [ $? -ne 0 ]
     then
         echo "yum install $package -y" &>> $LOGFILE
         VALIADTE $? "installation of $pacakage"
